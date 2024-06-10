3a8263d1442cbedcad07c969690f95cd0806c105
Tarang <tarangsultania54@gmail.com>
2024-06-10 13:46:57 +0530

    Completed project

--- Todo/src/features/todo/todoSlice.js
+++ Todo/src/features/todo/todoSlice.js
@@ -1,35 +1,28 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import {createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [{id: 1, text: "Hello World"}]
+    todos: [{id: 1, text: "Hello world"}]
 }
 
+
+
 export const todoSlice = createSlice({
-    name: "todo",
+    name: 'todo',
     initialState,
     reducers: {
-        addTodo: (state,action) => {
+        addTodo: (state, action) => {
             const todo = {
-                // id: 1,
-                // text: "Hello World"
-                id: nanoid(),
-                text: action.payload 
+                id: nanoid(), 
+                text: action.payload
             }
-            state.todos.push(todo);
+            state.todos.push(todo)
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
         },
-        updateTodo: (state, action) => {
-            const {id, text} = action.payload;
-            const todo = state.todos.find(todo => todo.id === id);
-            if(todo){
-                todo.text = text;
-            }
-        }
     }
 })
 
-export const {addTodo, removeTodo, updateTodo} = todoSlice.actions;
+export const {addTodo, removeTodo} = todoSlice.actions
 
 export default todoSlice.reducer
