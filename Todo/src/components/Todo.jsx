8bed12ec1e21de67981e71cf8640dc554d690567
Tarang <tarangsultania54@gmail.com>
2024-06-10 13:02:36 +0530

    Updated files

---
+++ Todo/src/components/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice' 
+
+function Todo() {
+    useSelector()
+  return (
+    <div>
+      <div>Todo</div>
+    </div>
+  )
+}
+
+export default Todo
--- Todo/src/features/todo/todoSlice.js
+++ Todo/src/features/todo/todoSlice.js
@@ -29,3 +29,7 @@ export const todoSlice = createSlice({
         }
     }
 })
+
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions;
+
+export default todoSlice.reducer
